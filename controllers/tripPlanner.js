efe4ada7d7d995df582538171cbeb02456394c8f
Babatunde Woodard-James <babatundewoodard-jamesCB5@MacBook-Air-8.local>
2019-08-26 07:04:26 -0400

    added more events function

---
+++ controllers/tripPlanner.js
@@ -0,0 +1,141 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const landmarksAPI = require('../models/landmarks.js')
+const eventsAPI = require('../models/events.js')
+const foodAPI = require('../models/food.js')
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const tripRouter = express.Router()
+const eventRouter = express.Router()
+
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+
+
+// Request handlers for events
+eventRouter.get('/events', (req, res) => {
+  eventsAPI.getAllEvents().then((events) =>{
+    res.send(events)
+  })
+})
+
+eventRouter.get('/events/:id', (req,res) => {
+  eventsAPI.getEvent(req.params.id).then((event) => {
+    res.render('events/event',{event})
+  })
+})
+
+eventRouter.get('/events/:id/edit', (req,res) => {
+  eventsAPI.getEvent(req.params.id).then((event) => {
+    res.render('events/editEvent',{event})
+  })
+})
+
+eventRouter.get('/events/new', (req,res) => {
+  res.render('newEntry.hbs',{})
+})
+
+// eventRouter.get('/events/:id', (req, res) => {
+//   eventsAPI.getEvent(req.params.id).then(() =>{
+//     res.send('Single Event')
+//   })
+// })
+
+eventRouter.post('/events', (req, res) => {
+  eventsAPI.addEvents(req.body).then(() => {
+    res.send(200)
+  })
+})
+
+// eventRouter.put('/events/:id', (req, res) => {
+//   eventsAPI.updateEvent(req.params.id, req.body).then(() =>{
+//     res.send('Update Event')
+//   })
+// })
+
+// eventRouter.delete('/events/delete', (req, res) => {
+//   eventsAPI.deleteEvent().then(() =>{
+//     res.send('Delete Event')
+//   })
+// })
+
+// issuesRouter.get('/:issueId', (req, res) => {
+//   issuesApi.getIssue(req.params.issueId).then((issue) => {
+//     res.render('issues/issue',{issue})
+//   })
+// })
+
+// issuesRouter.get('/:issueId/edit', (req, res) => {
+//   issuesApi.getIssue(req.params.issueId).then((issue) => {
+//     res.render('issues/editIssueForm',{issue})
+//   })
+// })
+
+// issuesRouter.put('/:issueId', (req,res) => {
+//   // console.log(req.params.issueId)
+//   // console.log(req.body)
+//   // issuesApi.updateIssue(req.params.issueId, req.body).then(
+//   //   () => {res.redirect(`/issues`)}
+//   // )
+
+//   console.log(req.params.issueId)
+//   console.log(req.body)
+//   issuesApi.updateIssue(req.params.issueId, req.body)
+//   .then( () => res.redirect(`/issues`))
+// })
+
+// issuesRouter.post('/', (req,res) => {
+//   issuesApi.addNewIssue(req.body)
+//   .then(res.redirect('/issues'))
+// })
+
+// issuesRouter.delete('/:issueId', (req,res) => {
+//   issuesApi.deleteIssue(req.params.issueId).then(res.redirect('/issues'))
+  
+// })
+
+
+
+
+
+/* Step 5
+ *
+ * TODO: delete this handler; it's just a sample
+ */ 
+
+
+/* Step 6
+ *
+ * Export the router from the file.
+ *
+ */
+module.exports = {
+  tripRouter,
+  eventRouter
+}
---
+++ models/events.js
@@ -0,0 +1,74 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const EventSchema = new mongoose.Schema({
+    name: String,
+    venue: String,
+    description: String,
+    date: Date,
+    price: Number,
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const EventCollection = mongoose.model('Events', EventSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+
+function getAllEvents() {
+    return EventCollection.find()
+}
+
+function getEvent(eventId) {
+    return EventCollection.findById(eventId)
+}
+
+function addEvents(newEvent) {
+    return EventCollection.insertMany([newEvent])
+}
+
+function updateEvent(eventId, updatedEvent) {
+    return EventCollection.findByIdAndUpdate({_id: eventId}, updatedEvent)
+}
+
+function deleteEvent(eventId) {
+    return EventCollection.findByIdAndDelete(eventId)
+}
+
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  addEvents,
+  deleteEvent,
+  getAllEvents,
+  getEvent,
+  updateEvent
+}
